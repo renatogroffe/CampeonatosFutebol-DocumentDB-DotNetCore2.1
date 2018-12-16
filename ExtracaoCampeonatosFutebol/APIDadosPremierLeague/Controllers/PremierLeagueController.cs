c7fd9aa70bd25d6d62466f6c2e5a61d2c15b31a9
Renato Groffe <renato.groffe@yahoo.com.br>
2018-12-16 19:02:42 -0200

    .NET Core 2.1 + Selenium WebDriver + Chrome Driver + DocumentDB
     .NET Core 2.1 + Selenium WebDriver + Chrome Driver + DocumentDB

---
+++ ExtracaoCampeonatosFutebol/APIDadosPremierLeague/Controllers/PremierLeagueController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Documents.Client;
+
+namespace APIDadosPremierLeague.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PremierLeagueController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(
+            [FromServices]DocumentDBConfigurations configurations)
+        {
+            using (var client = new DocumentClient(
+                new Uri(configurations.EndpointUri),
+                        configurations.PrimaryKey))
+            {
+                FeedOptions queryOptions =
+                    new FeedOptions { MaxItemCount = -1 };
+
+                return new ObjectResult(
+                    client.CreateDocumentQuery(
+                        UriFactory.CreateDocumentCollectionUri(
+                            configurations.Database,
+                            configurations.Collection),
+                        "SELECT c.id, c.NomeCampeonato, c.Esporte, c.Pais, " +
+                        "c.Equipes[0].Nome AS Lider, c.Equipes[19].Nome AS Ultimo " +
+                        "FROM c " +
+                        "WHERE c.NomeCampeonato = 'Premier League'",
+                        queryOptions).ToList());
+            }
+        }
+    }
+}
---
+++ ExtracaoCampeonatosFutebol/CargaCampeonatosFutebol/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace CargaCampeonatosFutebol
+{
+    class Program
+    {
+        private static SeleniumConfigurations _seleniumConfigurations;
+        private static DocumentDBConfigurations _documentDBConfigurations;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "*** Extração de Dados da Web com " +
+                ".NET Core 2.1, Selenium WebDriver e DocumentDB ***");
+            Console.WriteLine("Carregando configurações...");
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.json");
+            var configuration = builder.Build();
+
+            _seleniumConfigurations = new SeleniumConfigurations();
+            new ConfigureFromConfigurationOptions<SeleniumConfigurations>(
+                configuration.GetSection("SeleniumConfigurations"))
+                    .Configure(_seleniumConfigurations);
+
+            _documentDBConfigurations = new DocumentDBConfigurations();
+            new ConfigureFromConfigurationOptions<DocumentDBConfigurations>(
+                configuration.GetSection("DocumentDBConfigurations"))
+                    .Configure(_documentDBConfigurations);
+
+            ExtrairDadosCampeonato("Brasileirão", "Brasil",
+                _seleniumConfigurations.UrlPaginaClassificacaoBrasileirao);
+            ExtrairDadosCampeonato("La Liga", "Espanha",
+                _seleniumConfigurations.UrlPaginaClassificacaoLaLiga);
+            ExtrairDadosCampeonato("Premier League", "Inglaterra",
+                _seleniumConfigurations.UrlPaginaClassificacaoPremierLeague);
+
+            Console.WriteLine(
+                Environment.NewLine +
+                "Carga de dados concluída com sucesso!");
+
+            Console.ReadKey();
+        }
+
+        private static void ExtrairDadosCampeonato(
+            string nomeCampeonato, string pais, string urlClassificacao)
+        {
+            Console.Write(Environment.NewLine);
+            Console.WriteLine(
+                "Carregando driver do Selenium para Chrome em modo headless...");
+            var paginaClassificacao = new PaginaClassificacao(
+                _seleniumConfigurations,
+                nomeCampeonato, pais, urlClassificacao);
+
+            Console.WriteLine(
+                $"Carregando página com classificações - {nomeCampeonato}...");
+            paginaClassificacao.CarregarPagina();
+
+            Console.WriteLine(
+                "Extraindo dados...");
+            var classificacao = paginaClassificacao.ObterClassificacao();
+            paginaClassificacao.Fechar();
+
+            Console.WriteLine("Gravando dados extraídos...");
+            new ClassificacaoRepository(_documentDBConfigurations)
+                .Incluir(classificacao);
+        }
+    }
+}
